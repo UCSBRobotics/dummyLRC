a54b00eb19b65eab81081c79d6e514b73c1a8ac6
nullmiso –– SF <andrewyu6581@gmail.com>
2022-02-12 11:59:31 -0800

    working LRC

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 import sklearn
-#from sklearn.model_selection import train_test_split
-#x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size = 0.5, random_state = 0)
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+digits = load_digits()
+x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size = 0.5, random_state = 0)
+y_test = y_test.astype(bool).astype("i1")
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -19,4 +23,11 @@ class LRC:
                 #w_new = w_old + learn_rate(label-prediction)*input
                 self.weights += self.step_size * (label - self.predict(example)) * example
 
-    
+    def eval(self, test, labels):
+        correct = 0
+        for example, label in zip(test, labels):
+            p = self.predict(example)
+            p = 0 if p< 0.5 else 1
+            if p == label: 
+                correct += 1
+        print(correct, len(labels), correct/len(labels))
